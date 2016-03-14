e9fdbb231685296c6d297d234ed6db1c5c6a23d8
paiizz <wanchanapon.t@ku.th>
2016-03-14 18:40:21 +0700

    create models

---
+++ app/src/main/java/com/ske/snakebaddesign/models/Board.java
@@ -0,0 +1,8 @@
+package com.ske.snakebaddesign.models;
+
+/**
+ * Created by PaiizZ on 3/14/2016 AD.
+ */
+public class Board {
+
+}
---
+++ app/src/main/java/com/ske/snakebaddesign/models/Game.java
@@ -0,0 +1,7 @@
+package com.ske.snakebaddesign.models;
+
+/**
+ * Created by PaiizZ on 3/14/2016 AD.
+ */
+public class Game {
+}
